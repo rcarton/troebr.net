adc00e4dad3ee591290356e23928c48308f69e8c
Remi Carton <remi.carton@gmail.com>
2012-02-26 18:17:17 +0100

    fixed less build

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,7 @@ VERSION = datetime.datetime.now().strftime('%Y%m%d_%H%M')
 FILENAME = '%s_%s.tar.gz' % (APPNAME, VERSION)
 
 APPROOT = '/home/troebr/troebr.net'
+DEVROOT = os.path.dirname(os.path.abspath(__file__))
 
 # the user to use for the remote commands
 env.user = 'troebr'
@@ -32,12 +33,14 @@ def clup():
 
 def pack():
     # create a new source distribution as tarball
-    local('tar -czf dist/%s $(git ls-files)' % FILENAME, capture=False)
+    with cd(DEVROOT):
+        local('tar -czf dist/%s $(git ls-files)' % FILENAME, capture=False)
 
 def deploy():
     
     # upload the source tarball to the temporary folder on the server
-    put('dist/%s' % FILENAME, '/tmp/%s' % FILENAME)
+    with cd(DEVROOT):
+        put('dist/%s' % FILENAME, '/tmp/%s' % FILENAME)
     # create a place where we can unzip the tarball, then enter
     # that directory and unzip it
     run('rm -rf /tmp/%s && mkdir /tmp/%s' % (APPNAME, APPNAME))
